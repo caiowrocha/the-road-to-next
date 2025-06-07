8d08cccba33bac249bec407ed3a4e22dba337520
Caio Rocha <rochacaiodev@gmail.com>
2025-06-07 14:28:56 -0300

    feature: add simple icons to represent ticket status

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -2,16 +2,22 @@ import Link from "next/link"
 import { ticketPath } from "@/paths"
 import { initialTickets } from "../../data"
 
+const TICKET_ICONS = {
+    OPEN: "O",
+    DONE: "X",
+    IN_PROGRESS: ">"
+}
 
 const TicketsPage = () => {
     return (
         <div>
             {initialTickets.map((ticket) => (
                 <div key={ticket.id}>
+                    <div>{TICKET_ICONS[ticket.status]}</div>
                     <h2 className="text-lg">{ticket.title}</h2>
                     <Link href={ticketPath(ticket.id)}
                         className="underline text-sm"
-                        >
+                    >
                         View
                     </Link>
                 </div>
