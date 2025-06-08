443f050a274bee11f329f5c0e5dbb8d2ddfc3b0e
Caio Rocha <rochacaiodev@gmail.com>
2025-06-08 10:44:39 -0300

    feature: add some eslint rules

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,21 @@
-import Link from "next/link"
-import { ticketsPath } from "@/paths"
+import Link from 'next/link'
+import { ticketsPath } from '@/paths'
 
 const HomePage = () => {
   return (
-    <div>
-      <h2 className="text-lg">
-        Home Page
-      </h2>
+    <div className="flex-1 flex flex-col gap-y-8">
+      <div>
+        <h2 className="text-3xl font-bold tracking-tight">Home</h2>
+        <p className="text-sm text-muted-foreground">
+          Your home place to start
+        </p>
+      </div>
 
-      <Link
-        href={ticketsPath()} className="underline">
-      </Link>
+      <div className="flex-1 flex flex-col items-center">
+        <Link href={ticketsPath()} className="text-sm underline">
+          Go to Tickets
+        </Link>
+      </div>
     </div>
   )
 }
--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,27 +1,38 @@
-import Link from "next/link"
-import { ticketPath } from "@/paths"
-import { initialTickets } from "../../data"
+import Link from 'next/link'
+import { initialTickets } from '@/data'
+import { ticketPath } from '@/paths'
 
 const TICKET_ICONS = {
-    OPEN: "O",
-    DONE: "X",
-    IN_PROGRESS: ">"
+    OPEN: 'O',
+    IN_PROGRESS: '>',
+    DONE: 'X',
 }
 
 const TicketsPage = () => {
     return (
-        <div>
-            {initialTickets.map((ticket) => (
-                <div key={ticket.id}>
-                    <div>{TICKET_ICONS[ticket.status]}</div>
-                    <h2 className="text-lg">{ticket.title}</h2>
-                    <Link href={ticketPath(ticket.id)}
-                        className="underline text-sm"
+        <div className="flex-1 flex flex-col gap-y-8">
+            <div>
+                <h2 className="text-3xl font-bold tracking-tight">Tickets</h2>
+                <p className="text-sm text-muted-foreground">
+                    All your tickets at one place
+                </p>
+            </div>
+
+            <div className="flex-1 flex flex-col items-center gap-y-4">
+                {initialTickets.map((ticket) => (
+                    <div
+                        key={ticket.id}
+                        className="w-full max-w-[420px] p-4 border border-slate-100 rounded"
                     >
-                        View
-                    </Link>
-                </div>
-            ))}
+                        <div>{TICKET_ICONS[ticket.status]}</div>
+                        <h3 className="text-lg font-semibold truncate">{ticket.title}</h3>
+                        <p className="text-sm text-slate-500 truncate">{ticket.content}</p>
+                        <Link href={ticketPath(ticket.id)} className="text-sm underline">
+                            View
+                        </Link>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
