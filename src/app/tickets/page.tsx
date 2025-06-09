9652280faf44e15ea08b3f267eab5fc83ade3d73
Caio Rocha <rochacaiodev@gmail.com>
2025-06-09 00:29:59 -0300

    feature: add ticket-item component

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,15 +1,6 @@
-import { LucideCircleCheck, LucideFileText, LucidePencil } from 'lucide-react'
-import Link from 'next/link'
 import { Heading } from '@/components/heading'
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
 import { initialTickets } from '@/data'
-import { ticketPath } from '@/paths'
-
-const TICKET_ICONS = {
-    OPEN: <LucideFileText />,
-    IN_PROGRESS: <LucidePencil />,
-    DONE: <LucideCircleCheck />
-}
+import { TicketItem } from '@/features/tickets/components/ticket-item'
 
 const TicketsPage = () => {
     return (
@@ -21,27 +12,10 @@ const TicketsPage = () => {
 
             <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-from-top">
                 {initialTickets.map((ticket) => (
-                    <Card
+                    <TicketItem
                         key={ticket.id}
-                        className="w-full max-w-[420px]"
-                    >
-                        <CardHeader>
-                            <CardTitle className='flex gap-x-2'>
-                                <span>{TICKET_ICONS[ticket.status]}</span>
-                                <span className="truncate">{ticket.title}</span>
-                            </CardTitle>
-                        </CardHeader>
-                        <CardContent>
-                            <span className='line-clamp-3 whitespace-break-spaces'>
-                                {ticket.content}
-                            </span>
-                        </CardContent>
-                        <CardFooter>
-                            <Link href={ticketPath(ticket.id)} className="text-sm underline">
-                                View
-                            </Link>
-                        </CardFooter>
-                    </Card>
+                        ticket={ticket}
+                    />
                 ))}
             </div>
         </div>
